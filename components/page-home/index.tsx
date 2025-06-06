fb507105f164f9a435db34466da53388265d99d1
bybyers <byers.jacob@gmail.com>
2025-06-05 20:50:13 -0600

    fix: adjust main section height for improved layout

--- components/page-home/index.tsx
+++ components/page-home/index.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
 
   return (
     <>
-      <main className="flex flex-col min-h-[calc(100vh-68px)] justify-between items-center py-12 lg:py-24 2xl:pt-48">
+      <main className="flex flex-col min-h-[calc(80vh)] justify-between items-center py-12 lg:py-24 2xl:pt-48">
         <section className='controller-block w-full px-5 flex justify-center'>
           <div className='w-full max-w-xl flex flex-col items-center gap-y-8'>
             {nav?.items?.length ? (
@@ -36,7 +36,7 @@ export default function HomePage() {
           </div>
         </section>
         <div className="w-full text-center text-xs text-muted-foreground mt-8 mb-4">
-          Made with love by Jacob Byers &rarr;{" "}
+          Made with ❤️ by Jacob Byers &rarr;{" "}
           <Link href="https://www.ohmni.tech" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary transition">
             www.ohmni.tech
           </Link>
